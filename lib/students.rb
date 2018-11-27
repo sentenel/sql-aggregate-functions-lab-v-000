b6ed2e1876a1e36c82b0e8774e23c62ecdd587ca
Seth Wax <sethlwax@gmail.com>
2018-11-27 13:06:17 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks.
-require 'pry'
+
 def highest_student_gpa
-  binding.pry
+  'SELECT MAX(gpa) FROM students'
 end
 
 def lowest_student_gpa
